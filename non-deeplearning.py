7b5e0127ac2b343acb0978088fbe238c4cbd110f
genez <metegenez@gmail.com>
2019-10-21 13:29:36 +0300

    tracker objesi güncellendi
    içinde son contour bilgisini tutmasını ve associationda direk bunu çağırması sağlandı.

--- non-deeplearning.py
+++ non-deeplearning.py
@@ -5,12 +5,23 @@ import cv2
 import numpy as np
 from sklearn.cluster import DBSCAN
 import pandas as pd
+from tracking import *
 
 
 
 def detector(image):
+    """
+    This function uses adaptive filtering for segmentation of each cell which is a straigthforward task for such an
+    easy background.
+    :param image: .png file.
+    :return:
+    """
     def color_filtering(image):
-        original_image = image
+        """
+
+        :param image:
+        :return:
+        """
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         th3 = cv2.adaptiveThreshold(gray, 1, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                     cv2.THRESH_BINARY_INV, 51, 6)
@@ -24,6 +35,20 @@ def detector(image):
         cv2.drawContours(image, selected_contours, -1, (0, 255, 0), 3)
         plt.imshow(image)
         return selected_contours
+    return color_filtering(image)
+
+png_path = r"C:\Users\mete\Documents\Github-Rep\medical-tracking\data\png"
+valid_images = [".png"]
+sort_algorithm = Sort()
+debug_tracks = []
+for f in os.listdir(png_path):
+    ext = os.path.splitext(f)[1]
+    if ext.lower() not in valid_images:
+        continue
+    img = cv2.imread(os.path.join(png_path,f))
+    dets = detector(img)
+    debug_tracks.append(sort_algorithm.update(dets))
+
 
 
 
--- tracking.py
+++ tracking.py
@@ -23,9 +23,49 @@ def contours_to_z(contour):
     :param contour: Output of cv2.findcontours() function
     :return: z = [x,y] observables
     """
-    return np.mean(contour, axis=0)
+    return np.transpose(np.mean(contour, axis=0))
 
-class KalmanBoxTracker(object):
+def associate_detections_to_trackers(detections, trackers, iou_threshold=0.3):
+    """
+    Assigns detections to tracked object (both represented as bounding boxes)
+    Returns 3 lists of matches, unmatched_detections and unmatched_trackers
+    """
+    if (len(trackers) == 0):
+        return np.empty((0, 2), dtype=int), np.arange(len(detections)), np.empty((0, 5), dtype=int)
+    iou_matrix = np.zeros((len(detections), len(trackers)), dtype=np.float32)
+
+    for d, det in enumerate(detections):
+        for t, trk in enumerate(trackers):
+            iou_matrix[d, t] = iou_contours(det, trk.cont)
+    matched_indices = linear_assignment(-iou_matrix)
+
+    unmatched_detections = []
+    for d, det in enumerate(detections):
+        if (d not in matched_indices[:, 0]):
+            unmatched_detections.append(d)
+    unmatched_trackers = []
+    for t, trk in enumerate(trackers):
+        if (t not in matched_indices[:, 1]):
+            unmatched_trackers.append(t)
+
+    # filter out matched with low IOU
+    matches = []
+    for m in matched_indices:
+        if (iou_matrix[m[0], m[1]] < iou_threshold):
+            unmatched_detections.append(m[0])
+            unmatched_trackers.append(m[1])
+        else:
+            matches.append(m.reshape(1, 2))
+    if (len(matches) == 0):
+        matches = np.empty((0, 2), dtype=int)
+    else:
+        matches = np.concatenate(matches, axis=0)
+
+    return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
+
+
+
+class KalmanBoxTracker():
     """
     This class represents the internal state of individual tracked objects observed as origin of the contour.
     """
@@ -54,6 +94,7 @@ class KalmanBoxTracker(object):
         self.hits = 0
         self.hit_streak = 0
         self.age = 0
+        self.cont = cont
 
     def update(self, cont):
         """
@@ -85,3 +126,58 @@ class KalmanBoxTracker(object):
         """
         return self.kf.x
 
+class Sort(object):
+    def __init__(self, max_age=1, min_hits=3):
+        """
+        Sets key parameters for SORT
+        """
+        self.max_age = max_age
+        self.min_hits = min_hits
+        self.trackers = []
+        self.frame_count = 0
+
+    def update(self, dets):
+        """
+        Params:
+          dets - a numpy array of detections in the format [[x1,y1,x2,y2,score],[x1,y1,x2,y2,score],...]
+        Requires: this method must be called once for each frame even with empty detections.
+        Returns the a similar array, where the last column is the object ID.
+        NOTE: The number of objects returned may differ from the number of detections provided.
+        """
+        self.frame_count += 1
+        # get predicted locations from existing trackers.
+        # trks = np.zeros((len(self.trackers),2))
+        # to_del = []
+        ret = []
+        # for t, trk in enumerate(trks):
+        #     pos = self.trackers[t].predict()
+        #     trk[:] = [pos[0], pos[1]]
+        #     if (np.any(np.isnan(pos))):
+        #         to_del.append(t)
+        # trks = np.ma.compress_rows(np.ma.masked_invalid(trks))
+        # for t in reversed(to_del):
+        #     self.trackers.pop(t)
+        matched, unmatched_dets, unmatched_trks = associate_detections_to_trackers(dets, self.trackers)
+
+        # update matched trackers with assigned detections
+        for t, trk in enumerate(self.trackers):
+            if (t not in unmatched_trks):
+                d = matched[np.where(matched[:, 1] == t)[0], 0]
+                trk.update(dets[d])
+
+        # create and initialise new trackers for unmatched detections
+        for i in unmatched_dets:
+            trk = KalmanBoxTracker(dets[i])
+            self.trackers.append(trk)
+        i = len(self.trackers)
+        for trk in reversed(self.trackers):
+            d = trk.get_state()[:2]
+            if ((trk.time_since_update < 1) and (trk.hit_streak >= self.min_hits or self.frame_count <= self.min_hits)):
+                ret.append(np.concatenate((d, [trk.id + 1])).reshape(1, -1))  # +1 as MOT benchmark requires positive
+            i -= 1
+            # remove dead tracklet
+            if (trk.time_since_update > self.max_age):
+                self.trackers.pop(i)
+        if (len(ret) > 0):
+            return np.concatenate(ret)
+        return np.empty((0, 5))
